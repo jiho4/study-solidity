3b70fe676ea0915f0688f14f6c192f3a95a6d670
jiho4 <jiho4774@gmail.com>
2022-12-10 23:57:51 -0500

    add supply chain sample code

---
+++ test/supply_chain.js
@@ -0,0 +1,35 @@
+var SupplyChain = artifacts.require('./SupplyChain.sol');
+
+contract('SupplyChain', async accounts => {
+  it("should create a Participant", async () => {
+    let instance = await SupplyChain.deployed();
+    let participantId = await instance.addParticipant("A","passA","0xa220013D141c725C32210415AAd4819A28FD749f","Manufacturer");
+    let participant = await instance.participants(0);
+    assert.equal(participant[0], "A");
+    assert.equal(participant[2], "Manufacturer");
+
+    participantId = await instance.addParticipant("B","passB","0x0De8695e69C77DaC48b3Ef14743a61f99c25dDC5","Supplier");
+    participant = await instance.participants(1);
+    assert.equal(participant[0], "B");
+    assert.equal(participant[2], "Supplier");
+
+    participantId = await instance.addParticipant("C","passC","0x41F10e0F641C04E5aA325AF7435A10B57d22b762","Consumer");
+    participant = await instance.participants(2);
+    assert.equal(participant[0], "C");
+    assert.equal(participant[2], "Consumer");
+  });
+
+  it("should return Participant details", async () => {
+    let instance = await SupplyChain.deployed();
+    let participantDetails = await instance.getParticipant(0);
+    assert.equal(participantDetails[0], "A");
+
+    instance = await SupplyChain.deployed();
+    participantDetails = await instance.getParticipant(1);
+    assert.equal(participantDetails[0], "B");
+
+    instance = await SupplyChain.deployed();
+    participantDetails = await instance.getParticipant(2);
+    assert.equal(participantDetails[0], "C");
+  })
+});
